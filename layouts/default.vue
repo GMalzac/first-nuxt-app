4f43241011120ee9fa683f8532271ade89566603
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-04-23 11:14:17 +0100

    Add a navbar to all pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
+  <div class="container">
+    <TheNavbar/>
+
     <nuxt/>
   </div>
 </template>
 
+<script>
+  import TheNavbar from '@/components/TheNavbar'
+  export default {
+    components: {
+      TheNavbar
+    }
+  }
+</script>
+
 <style>
+
 
 </style>
